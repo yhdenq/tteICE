2311d55ba8df2b18f27908eddf840bffe3705aa2
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-02-17 13:29:34 +0800

    Create print_summary_tteICE.R

---
+++ R/print_summary_tteICE.R
@@ -0,0 +1,8 @@
+#' @title Print the summary of 'tteICE'
+#' @description Print the summary of 'tteICE'
+#' @param object A fitted object returned by the function \code{tteICE}, \code{surv.tteICE}, or \code{scr.tteICE}.
+#' @keywords internal
+
+print.summary.tteICE <- function(object, ...) {
+  print(object)
+}
