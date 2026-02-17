4291c52f38159157b2df582412dc9b0af6c35b50
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-02-17 13:39:40 +0800

    Update print_summary_tteICE.R

--- R/print_summary_tteICE.R
+++ R/print_summary_tteICE.R
@@ -3,6 +3,20 @@
 #' @param object A fitted object returned by the function \code{tteICE}, \code{surv.tteICE}, or \code{scr.tteICE}.
 #' @keywords internal
 
-print.summary.tteICE <- function(object, ...) {
-  print(object)
+print.summary.tteICE <- function(x) {
+  if(!is.null(x$call)){
+    cat("Input:\n")
+    print(x$call)
+  }
+  cat("-----------------------------------------------------------------------\n")
+  cat("Data type:", x$dtype, "\n")
+  cat("Strategy:", x$strategy, "\n")
+  cat("Estimation method:", x$method, "\n")
+  cat("Observations:", x$n, '(including', x$n1, 'treated and', x$n0, 'control)\n')
+  cat("Maximum follow-up time:", x$maxt, '\n')
+  cat("P-value of the average treatment effect:", round(x$p.val, 3), "\n")
+  cat("-----------------------------------------------------------------------\n")
+  cat("The estimated cumulative incidences and treatment effects at quartiles:\n")
+  print(round(x$est, 3))
+  cat("\n")
 }
