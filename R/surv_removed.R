3e961705fac55f2906ebfc9ff32343a60978885d
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-02-19 20:58:57 +0800

    Update surv_removed.R

--- R/surv_removed.R
+++ R/surv_removed.R
@@ -63,13 +63,13 @@ surv.removed <- function(A,Time,cstatus,weights=rep(1,length(A))){
   tt = sort(unique(c(tt1,tt0)))
   cumhaz1 = c(0, fit1$cumhaz)
   cumhaz0 = c(0, fit0$cumhaz)
-  cumhaz = data.frame(time=tt,cumhaz1=cumhaz1,cumhaz0=cumhaz0)
   cif1 = 1 - exp(-cumhaz1)
   cif0 = 1 - exp(-cumhaz0)
   se1 = c(0, fit1$std.err * fit1$surv)
   se0 = c(0, fit0$std.err * fit0$surv)
   se1[is.na(se1)] = rev(na.omit(se1))[1]
   se0[is.na(se0)] = rev(na.omit(se0))[1]
+  cumhaz = data.frame(time=tt,cumhaz1=.matchy(cumhaz1,tt1,tt),cumhaz0=.matchy(cumhaz0,tt0,tt))
   surv_diff = survdiff(Surv(Time,cstatus==1)~A)
   p = pchisq(surv_diff$chisq, length(surv_diff$n)-1, lower.tail=FALSE)
   ate = .matchy(cif1,tt1,tt)-.matchy(cif0,tt0,tt)
