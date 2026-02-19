bfe6afd8588ebdd21f12817a247fae2340e78ea3
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-02-19 13:51:41 +0800

    Update scr_removed.R

--- R/scr_removed.R
+++ R/scr_removed.R
@@ -53,7 +53,7 @@
 #' @seealso \code{\link[tteICE]{scr.removed.eff}}, \code{\link[tteICE]{scr.tteICE}}
 #'
 #'
-#' @export
+#' @keywords internal
 
 scr.removed <- function(A,Time,status,Time_int,status_int,weights=rep(1,length(A))){
   Time = (Time + Time_int - abs(Time-Time_int))/2
