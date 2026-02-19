597ff753e1b0f18a76152c323d654239a53c2425
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-02-19 13:59:45 +0800

    Update surv_treatment_eff.R

--- R/surv_treatment_eff.R
+++ R/surv_treatment_eff.R
@@ -27,6 +27,8 @@
 #' \item{se}{Standard error of the estimated treatment effect.}
 #' \item{p.val}{P value of testing the treatment effect based on the efficient influence function
 #' of the restricted mean survival time lost by the end of study.}
+#' \item{coef}{Coefficients of covariates in the working Cox models for the primary event.}
+#' \item{ph}{P values of the proportional hazards assumption in the working Cox models for the primary event.}
 #' }
 #'
 #' @details
@@ -55,7 +57,7 @@
 #' @seealso \code{\link[tteICE]{surv.treatment}}, \code{\link[tteICE]{surv.tteICE}}
 #'
 #'
-#' @export
+#' @keywords internal
 
 surv.treatment.eff <- function(A,Time,cstatus,X=NULL){
   n = length(A)
