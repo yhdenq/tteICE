fbacd95ca588a2ec4ac45a3f2fc695b15896e9d5
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-02-19 14:09:26 +0800

    Update coef_tteICE.R

--- R/coef_tteICE.R
+++ R/coef_tteICE.R
@@ -5,7 +5,17 @@
 #' @param object 
 #' A fitted object returned by the function \code{tteICE}, \code{surv.tteICE}, or \code{scr.tteICE}.
 #'
-#' @keywords internal
+#' @return
+#' A list of coefficients of covariates in the working Cox models, stratified by treatment groups. 
+#' For the treatment policy strategy and composite variable strategy, only one Cox model is fit (for the primary 
+#' outcome event or the composite event). In these two strategies, \code{coef1} is the coefficients in the treated 
+#' group, \code{coef0} is the coefficients in the control group. For other strategies, Cox models are fitted for 
+#' each event (primary outcome event and intercurrent event). In these strategies, \code{coef11} is the coefficients 
+#' for the primary outcome event in the treatment group, \code{coef10} is the coefficients for the primary outcome 
+#' event in the control group, \code{coef21} is the coefficients for the inercurrent event in the treated group, 
+#' \code{coef20} is the coefficients for the intercurrent in the control group.
+#'
+#' @export
 
 coef.tteICE <- function(object) {
   object$coef
