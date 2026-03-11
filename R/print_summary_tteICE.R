1a9464a9b8f76fdff4a568ec87b1186451fd82ab
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-03-11 10:35:18 +0800

    Update print_summary_tteICE.R

--- R/print_summary_tteICE.R
+++ R/print_summary_tteICE.R
@@ -23,6 +23,27 @@ print.summary.tteICE <- function(x, digits=3, ...) {
   cat("Observations:", x$n, '(including', x$n1, 'treated and', x$n0, 'control)\n')
   cat("Maximum follow-up time:", round(x$maxt, digits), '\n')
   cat("P-value of the average treatment effect:", round(p, digits), "\n")
+  if (!is.null(x$coef)) {
+    cat("-----------------------------------------------------------------------\n")
+    coef1 = rbind(x$coef$coef11, x$coef$coef10)
+    if (!is.null(coef1)){
+      rownames(coef1) = c("A=1", "A=0")
+      cat("Coefficients of covariates in the Cox model for event 1\n")
+      cat(coef1,"\n")
+    }
+    coef2 = rbind(x$coef$coef21, x$coef$coef20)
+    if (!is.null(coef2)){
+      rownames(coef2) = c("A=1", "A=0")
+      cat("Coefficients of covariates in the Cox model for event 2\n")
+      cat(coef2,"\n")
+    }
+    coef = rbind(x$coef$coef1, x$coef$coef0)
+    if (!is.null(coef)){
+      rownames(coef) = c("A=1", "A=0")
+      cat("Coefficients of covariates in the Cox model\n")
+      cat(coef,"\n")
+    }
+  }
   cat("-----------------------------------------------------------------------\n")
   cat("The estimated cumulative incidences and treatment effects at quartiles:\n")
   print(round(x$est, digits))
