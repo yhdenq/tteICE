44194d80681cc90412dcd9a057d21d30dea3f9cb
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-08-14 15:06:50 -0700

    Update surv_boot.R

--- R/surv_boot.R
+++ R/surv_boot.R
@@ -10,7 +10,7 @@
 #' A fitted object returned by the function \code{tteICE}, \code{surv.tteICE}, or \code{scr.tteICE}.
 #'
 #' @param nboot
-#' Number of resamplings in the boostrapping method. If \code{nboot} is 0 or 1, then
+#' Number of resamplings in the bootstrapping method. If \code{nboot} is 0 or 1, then
 #' asymptotic standard error based on the explicit form is calculated instead of bootstrapping.
 #'
 #' @param seed Seed for bootstrapping.
