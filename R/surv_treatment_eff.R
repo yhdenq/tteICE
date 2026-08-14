7720cfeb444ce4bbcb635d03ea8f646bb4c41cb3
yhdenq <152732769+yhdenq@users.noreply.github.com>
2026-08-14 15:04:47 -0700

    Update surv_treatment_eff.R

--- R/surv_treatment_eff.R
+++ R/surv_treatment_eff.R
@@ -65,7 +65,7 @@ surv.treatment.eff <- function(A,Time,cstatus,X=NULL){
     return(surv.treatment(A,Time,cstatus))
   }
   X = as.matrix(scale(X))
-  if (is.null(colnames(X)) {
+  if (is.null(colnames(X))) {
     if (ncol(X)==1) {
       colnames(X) = "X"
     } else {
